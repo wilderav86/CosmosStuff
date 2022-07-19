eb44a672f3684759e0634aadde6a295cfeeba043
Adam Wilder <wilderav86@gmail.com>
2022-07-18 22:15:27 -0700

    new iss data api endpoint

--- components/IssMap.js
+++ components/IssMap.js
@@ -10,32 +10,35 @@ const IssMap = () => {
     zoom: 0,
   });
 
-  const url = "http://api.open-notify.org/iss-now.json/";
-
-  const [issLocation, setIssLocation] = useState({});
+  const [issData, setIssData] = useState({});
   const [loading, setLoading] = useState(true);
 
+  const url = "https://api.wheretheiss.at/v1/satellites/25544";
   useEffect(() => {
-    const fetchIssLocation = async () => {
+    const fetchIssData = async () => {
       setLoading(true);
       const response = await fetch(url);
       const json = await response.json();
-      setIssLocation(json);
+      setIssData(json);
       console.log(json);
       setLoading(false);
     };
 
-    fetchIssLocation();
+    fetchIssData();
 
     const interval = setInterval(() => {
-      fetchIssLocation();
-    }, 5000);
+      fetchIssData();
+    }, 1000);
     return () => clearInterval(interval);
   }, []);
 
   return (
     <div>
-      hello
+      <div>
+        <p>alt: {issData.altitude}</p>
+        <p>speed: {issData.velocity} kph</p>
+      </div>
+
       <Map
         {...viewState}
         mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
@@ -45,8 +48,8 @@ const IssMap = () => {
       >
         {!loading && (
           <Marker
-            longitude={issLocation.iss_position.longitude}
-            latitude={issLocation.iss_position.latitude}
+            longitude={issData.longitude}
+            latitude={issData.latitude}
             anchor="bottom"
           >
             <Image src="/ISS_01b.svg" height="60" width="60" />
