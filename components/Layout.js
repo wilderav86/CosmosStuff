401c15b29696120d96c30a57277f0d9921dbd1f2
Adam Wilder <wilderav86@gmail.com>
2022-08-02 18:18:24 -0700

    fixed deployment error

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,5 @@
 import Navbar from "../components/navbar/Navbar.js";
 import { useRouter } from "next/router";
-import InitialFadeIn from "../animations/InitialFadein.js";
 
 export default function Layout({ children }) {
   //Do not render navbar on home page.
