c23fbfbc976dc801f8cd1a2564c6a68b8df01d07
Adam Wilder <wilderav86@gmail.com>
2022-08-20 17:39:38 -0700

    styling changes and back next buttons added

--- pages/JamesWebb.js
+++ pages/JamesWebb.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import Zoom from "react-medium-image-zoom";
+import Loading from "../components/loading/Loading";
 
 import { useApi, useUrl } from "../hooks";
 import { headers } from "../next.config";
@@ -10,8 +11,16 @@ import styles from "../styles/pages/JamesWebb.module.scss";
 const JamesWebb = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
-  const url = "https://api.jwstapi.com/all/type/jpg?page=100&perPage=10";
+  //fetch suffix list
+  // const url = "https://api.jwstapi.com/suffix/list";
+
+  //
+  // const url = "https://api.jwstapi.com/all/suffix/_cal?page=1&perPage=10";
+
+  //fetch by jpg
+  const url = `https://api.jwstapi.com/all/type/jpg?page=${page}&perPage=10`;
   const headers = new Headers({
     "X-API-KEY": "353254d5-cf72-4648-b977-ba098e7f3a19",
   });
@@ -27,23 +36,35 @@ const JamesWebb = () => {
     };
 
     fetchData();
-  }, []);
+  }, [page]);
+
+  console.log(data);
 
   return (
     <div className={styles.pageContainer}>
+      <button onClick={() => setPage((page -= 1))}>Back</button>
+      <button onClick={() => setPage((page += 1))}>Next</button>
       {loading ? (
-        <div>loading</div>
+        <Loading />
       ) : (
-        data.body.map((image, key) => {
-          console.log("image", image.location);
-          return (
-            <Zoom key={key}>
-              <div className={styles.imageContainer}>
-                <Image src={image.location} layout="fill" objectFit="contain" />
-              </div>{" "}
-            </Zoom>
-          );
-        })
+        <div className={styles.imagesContainer}>
+          {data.body.map((image, key) => {
+            // console.log("image", image);
+            return (
+              <Zoom key={key}>
+                <div className={styles.imageContainer}>
+                  <Image
+                    src={image.location}
+                    height={260}
+                    width={250}
+                    layout="responsive"
+                    objectFit="contain"
+                  />
+                </div>{" "}
+              </Zoom>
+            );
+          })}
+        </div>
       )}
     </div>
   );
